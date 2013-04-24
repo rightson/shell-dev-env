a841df1ec301cd596b4632d891c9dcdb9dec32f4
Scott Chen <rightson@gmail.com>
2013-04-24 09:49:31 +0800

    Update template and extract deploy script

--- deploy.sh
+++ deploy.sh
@@ -11,20 +11,36 @@ SCREENRC=$HOME/.screenrc
 
 FILES=($BASHRC $VIMRC $SCREENRC)
 
-for each in $FILES[@]; do
-    if [ ! -f $each ]; then
-        touch $each
+function apply_patch() {
+
+    echo 'Patching...'
+    for each in $FILES[@]; do
+        if [ ! -f $each ]; then
+            echo "Creating rc file $each"
+            touch $each
+        fi
+    done
+    
+    if [ -z "`grep \"$IDENTIFIER\" $BASHRC`" ]; then
+        echo "Patching bashrc: $BASHRC"
+        cat rc/bashrc >> $BASHRC
     fi
-done
+    
+    if [ -z "`grep \"$IDENTIFIER\" $VIMRC`" ]; then
+        echo "Patching vimrc: $BASHRC"
+        cat rc/vimrc >> $VIMRC
+    fi
+    
+    if [ -z "`grep \"$IDENTIFIER\" $SCREENRC`" ]; then
+        echo "Patching screenrc: $BASHRC"
+        cat rc/screenrc >> $SCREENRC
+    fi
+    
+    echo 'Done!'
+}
 
-if [ -z "`grep \"$IDENTIFIER\" $BASHRC`" ]; then
-    cat rc/bashrc >> $BASHRC
-fi
-
-if [ -z "`grep \"$IDENTIFIER\" $VIMRC`" ]; then
-    cat rc/vimrc >> $VIMRC
-fi
-
-if [ -z "`grep \"$IDENTIFIER\" $SCREENRC`" ]; then
-    cat rc/screenrc >> $SCREENRC
-fi
+case $ARGV[1] in
+    *)
+        apply_patch
+    ;;
+esac
