431bf77bda97264514a4df49874ce1abf8fcd054
Scott Chen <rightson@gmail.com>
2018-02-04 12:56:44 +0000

    Add antigen default settings

---
+++ env/zsh/antigen.zsh
@@ -0,0 +1,15 @@
+source ~/.antigen.zsh
+
+antigen use oh-my-zsh
+
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-completions
+antigen bundle zsh-users/zsh-autosuggestions
+
+antigen bundle git
+antigen bundle npm
+antigen bundle pip
+antigen bundle colorize
+antigen theme robbyrussell
+antigen apply
+
