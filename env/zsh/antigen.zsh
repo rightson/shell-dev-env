bb0839f26b7a3c81798a1973329dc27b777a46fb
Scott Chen <rightson@gmail.com>
2018-02-14 11:10:46 +0800

    Make antigen file apply only at once

--- env/zsh/antigen.zsh
+++ env/zsh/antigen.zsh
@@ -2,22 +2,24 @@ ANTIGEN=~/.antigen.zsh
 if [ ! -f $ANTIGEN ]; then
     curl -L git.io/antigen > $ANTIGEN
 fi
-source $ANTIGEN
 
-antigen use oh-my-zsh
+if [ -z `command -v antigen` ]; then
+    source $ANTIGEN
+    antigen use oh-my-zsh
 
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-completions
-antigen bundle zsh-users/zsh-autosuggestions
+    antigen bundle zsh-users/zsh-syntax-highlighting
+    antigen bundle zsh-users/zsh-completions
+    antigen bundle zsh-users/zsh-autosuggestions
 
-antigen bundle git
-antigen bundle npm
-antigen bundle pip
-antigen bundle colorize
-if [ `uname -s` = Darwin ]; then
-antigen theme apple
-else
-antigen theme robbyrussell
+    antigen bundle git
+    antigen bundle npm
+    antigen bundle pip
+    antigen bundle colorize
+
+    if [ `uname -s` = Darwin ]; then
+        antigen theme apple
+    else
+        antigen theme bira
+    fi
+    antigen apply
 fi
-antigen apply
-
