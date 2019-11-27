e8ea03de2555318f5401d14b3ffe39f9707b5d8b
Scott Chen <rightson@gmail.com>
2019-11-27 19:11:23 +0800

    Update rprompt of zsh

--- env/zsh/statusbar.zsh
+++ env/zsh/statusbar.zsh
@@ -29,26 +29,35 @@ cwd=%~
 
 root="%{$fg[red]%}$username%{$reset_color%}"
 user="%{$fg[green]%}$username%{$reset_color%}"
+
+user_at_host="%{$fg_bold[green]%}%n@%m"
+
 host="%{$fg[green]%}$hostname%{$reset_color%}"
 rpwd="%{$fg[yellow]%}%/%{$reset_color%}"
 opwd="%{$fg[yellow]%}$cwd%{$reset_color%}"
-date="%{$fg[red]%}%D%{$reset_color%}"
+
+get_cwd="%{$fg[white]%}[%~]%{$reset_color%}"
+
+get_now="%{$fg[blue]%}%D{[%X]}%{$reset_color%}"
+
+date="%{$fg[red]%}<%D>%{$reset_color%}"
 time="%{$fg[cyan]%}%*%{$reset_color%}"
 at="@"
+
+get_prompt=$'\n'"%{$fg[blue]%}->%{$fg_bold[blue]%} %#%{$reset_color%} "
+
 #gitbranch=$(git rev-parse --abbrev-ref HEAD)
 #gb="%{$fg[red]%}$gitbranch%{$reset_color%}"
-
-RPROMPT="[${date}|${time}]"
 
 ps1_root() {
     PS1="${root}@${host}[${cwd}]# "
 }
 
 ps1_pretty() {
+    #PROMPT="${user_at_host} ${get_now} ${get_cwd} $(git_prompt_info) ${date} ${get_prompt}"
+    RPROMPT="${date}"
     #PROMPT="${user}${at}${host}:${rpwd}"$'\n'"# "
     #PROMPT="${user}${at}${host}:${opwd}"$'\n'"# "
-    PROMPT="[${date}] ${PROMPT}"
-    #RPROMPT="[${date}|${time}]"
     #PROMPT="${user}${at}${host}:${rpwd}"$'\n'"➜  "
     #PS1="${user}${at}${host}:${rpwd}[${date}]"$'\n'"➜  "
     #PS1="${user}${at}${host}:${rpwd}"$'\n'"[${date}] ➜  "
@@ -73,5 +82,5 @@ if [ "`id -u`" -eq 0 ]; then
     ps1_root
 else
     ps1_pretty
-fi 
+fi
 
