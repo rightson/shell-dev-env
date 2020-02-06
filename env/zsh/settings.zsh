851c7d4e835f0562df67fa689b982811edcc323d
Scott Chen <rightson@gmail.com>
2020-02-06 09:48:31 +0800

    Add $HOME/.local/bin to $PATH

--- env/zsh/settings.zsh
+++ env/zsh/settings.zsh
@@ -30,6 +30,6 @@ vimode() {
 vimode
 
 export ENV_PATH=$HOME/.env
-export PATH=$HOME/local/bin:$ENV_PATH/bin:/usr/local/sbin:/usr/local/bin:$PATH
+export PATH=$HOME/local/bin:$HOME/.local/bin:$ENV_PATH/bin:/usr/local/sbin:/usr/local/bin:$PATH
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=fg=cyan
 export LC_ALL=${LANG}
