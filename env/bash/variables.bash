55016dc7b1da848d4ca70aa9130d63db797bdc62
Scott Chen <rightson@gmail.com>
2020-11-28 14:29:00 +0800

    Remove legacy env folders

--- env/bash/variables.bash
+++
@@ -1,5 +0,0 @@
-if [[ `uname` = 'Linux' ]]; then
-    export PROFILE='~/.bashrc'
-else # Darwin
-    export PROFILE='~/.bash_profile'
-fi
--- env/zsh/variables.zsh
+++
@@ -1,7 +0,0 @@
-if [[ `uname` = 'Linux' ]]; then
-    export PROFILE='~/.zshrc'
-else # Darwin
-    export PROFILE='~/.zshrc'
-fi
-
-export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=fg=cyan
