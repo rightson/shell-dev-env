0ed74e8b3353634c935b83a591dc524ef553738f
Scott Chen <scott.chen@mediatek.com>
2020-11-30 17:20:33 +0800

    Fix typo

--- inc/fzf.sh
+++ inc/fzf.sh
@@ -1,6 +1,6 @@
 # FZF
 
-function fzf_use_fg() {
+function fzf_use_rg() {
     export FZF_CTRL_T_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*" --glob "!node_modules/*" --glob "!vendor/*" 2> /dev/null'
     export FZF_DEFAULT_COMMAND=$FZF_CTRL_T_COMMAND
 }
