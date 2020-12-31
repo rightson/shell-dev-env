946a0d80261fd107f6656801d3bdd53d26381bcc
Scott Chen <rightson@gmail.com>
2020-12-31 21:08:22 +0800

    Handling file path containing spaces case

--- inc/pdf.sh
+++ inc/pdf.sh
@@ -1,14 +1,14 @@
 function strip_pdf_annotations() {
-    local original=$1
-    local name=${original%%.*}
-    local ext=${original#*.}
+    local original="$1"
+    local name="${original%%.*}"
+    local ext="${original#*.}"
     local uncompressed=`mktemp`.pdf
     local stripped=`mktemp`.pdf
-    local output=$name-strpped.$ext 
-    echo "Uncompressing $original ..."
-    pdftk $original output $uncompressed uncompress
-    echo "Striping $original ..."
+    local output="$name-strpped.$ext"
+    echo "Uncompressing "$original" ..."
+    pdftk "$original" output $uncompressed uncompress
+    echo "Striping "$original" ..."
     LANG=C sed -n '/^\/Annots/!p' $uncompressed > $stripped
-    echo "Saving strpped $original as $output ..."
-    pdftk $stripped output $output compress
+    echo "Saving strpped "$original" as $output ..."
+    pdftk $stripped output "$output" compress
 }
