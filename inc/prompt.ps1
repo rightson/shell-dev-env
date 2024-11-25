09bb9b61fc0d7028871bcaf31bed636834d598c0
Scott Chen <rightson@gmail.com>
2024-11-25 14:41:40 +0800

    Add git branch info to prompt

--- inc/prompt.ps1
+++ inc/prompt.ps1
@@ -20,15 +20,30 @@ function prompt {
     # Set the window title
     $host.UI.RawUI.WindowTitle = "PowerShell - $username@$hostname : $currentLocation"
 
+    # Get Git branch and status
+    $gitInfo = ""
+    if (Get-Command git -ErrorAction SilentlyContinue) {
+        $gitBranch = git rev-parse --abbrev-ref HEAD 2>$null
+        if ($gitBranch) {
+            $gitStatus = git status --porcelain
+            $gitInfo = " ($gitBranch"
+            if ($gitStatus) {
+                $gitInfo += "*"
+            }
+            $gitInfo += ")"
+        }
+    }
+
     # Build the prompt string with colors
     $promptString = ""
     $promptString += Write-Host "$username" -ForegroundColor Red -NoNewline
     $promptString += Write-Host "@" -ForegroundColor Blue -NoNewline
     $promptString += Write-Host "$hostname" -ForegroundColor Green -NoNewline
-    #$promptString += Write-Host ":$relativePath" -ForegroundColor Yellow -NoNewline
     $promptString += Write-Host ":$currentLocation" -ForegroundColor Yellow -NoNewline
     $promptString += Write-Host " [$date $time]" -ForegroundColor Magenta -NoNewline
-    # $promptString += Write-Host " ($shell)" -ForegroundColor Blue -NoNewline
+    if ($gitInfo) {
+        $promptString += Write-Host $gitInfo -ForegroundColor Cyan -NoNewline
+    }
     $promptString += Write-Host "`n$" -NoNewline
 
     return " "
